c060e6f1e2aec0c3b243edbd00ab5f003db4a499
aditya <singhaditya2869@gmail.com>
2024-07-30 21:19:28 +0530

    user resolver

--- services/help-desk/src/entities/user.entity.ts
+++ services/help-desk/src/entities/user.entity.ts
@@ -1,7 +1,38 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity({ name: "users" })
 export class UserEntity extends BaseEntity {
+  @Field()
   @PrimaryGeneratedColumn("uuid")
   id!: string;
+
+  @Field()
+  @Column({name: "user_name"})
+  userName?: string
+  
+  @Field()
+  @Column()
+  email?:  string
+
+  @Field()
+  @Column()
+  password?: string
+
+  @Field()
+  @Column({name: "phone_Number"})
+  phoneNumber?: string
+
+  @Field()
+  @CreateDateColumn({
+    type: 'time with time zone'
+  })
+  createdAt?: Date
+
+  @Field()
+  @CreateDateColumn({
+    type: "time with time zone"
+  })
+  updatedAt?: Date
 }
--- services/help-desk/src/resolvers/user.resolver.ts
+++ services/help-desk/src/resolvers/user.resolver.ts
@@ -1,9 +1,78 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { UserEntity } from "../entities/user.entity"
+import { error } from "console";
 
-@Resolver()
+@InputType()
+export class CreateUserInput {
+  @Field()
+  userName?: string;
+
+  @Field()
+  email?: string;
+
+  @Field()
+  password?: string;
+
+  @Field()
+  phoneNumber?: string;
+}
+
+@InputType()
+export class UpdateUserInput {
+  @Field({ nullable: true })
+  userName?: string;
+
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  password?: string;
+
+  @Field({ nullable: true })
+  phoneNumber?: string;
+}
+
+@Resolver() 
 export class UserResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return "Hello world";
+  
+  @Query(() => [UserEntity])
+  async getAllUsers(): Promise<UserEntity[]>{
+    const user  = await UserEntity.find()
+    return user
+  }
+
+  @Mutation(() => UserEntity)
+  async createUser(
+    @Arg("data") data: CreateUserInput
+  ):Promise<UserEntity> {
+    const user = UserEntity.create(data as UserEntity)
+    await user.save()
+    return user
+  }
+
+  @Mutation(() => UserEntity)
+  async userUpdate(
+    @Arg("id") id: string,
+    @Arg("data") data: UpdateUserInput
+  ):Promise<UserEntity |  null>{
+    const user = await UserEntity.findOne({ where: {id} });
+    if(!user){
+      throw new Error("user are not found")
+    }
+    Object.assign(user, data)
+    await user.save()
+    return user
+  }
+
+  @Mutation(() =>  Boolean)
+  async deleteUser(
+    @Arg("id") id: string
+  ): Promise<boolean>{
+    const user = await UserEntity.findOne({where: {id} })
+    if(!user){
+      throw new Error("user are not found")
+    }
+    await user.remove();
+    return true
   }
 }
